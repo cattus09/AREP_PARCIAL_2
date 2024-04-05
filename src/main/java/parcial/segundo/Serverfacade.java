4d10945be4fde3531cc4239e4d0efff8ae2edad5
cattus09 <sergio.gonzalez-v@mail.escuelaing.edu.co>
2024-04-04 23:04:10 -0500

    mejora para AWS

--- src/main/java/parcial/segundo/Serverfacade.java
+++ src/main/java/parcial/segundo/Serverfacade.java
@@ -1,9 +1,6 @@
 package parcial.segundo;
 
 import static spark.Spark.*;
-//https://amazonxxx.x.xxx.x.xxx:{port}/linearsearch?list=10,20,13,40,60&value=13
-//https://amazonxxx.x.xxx.x.xxx:{port}/binarysearch?list=10,20,13,40,60&value=13
-//http://localhost:5000/binarysearch?list=10,20,13,40,60&value=13
 
 public class Serverfacade {
     static HttpConnetionExample conection = new HttpConnetionExample();
@@ -17,7 +14,7 @@ public class Serverfacade {
             String rest;
             String value = req.queryParamsValues("value")[0];
             String list = req.queryParamsValues("list")[0];
-            String[] conString = {"http://localhost:5000/binarysearch?list="+ list +"&value=" + value, "http://localhost:5000/binarysearch?list="+ list +"&value=" + value};
+            String[] conString = {"http://ec2-user@ec2-3-90-110-121.compute-1.amazonaws.com:5000/binarysearch?list="+ list +"&value=" + value, "http://ec2-user@ec2-107-21-15-250.compute-1.amazonaws.com:5000/binarysearch?list="+ list +"&value=" + value};
             System.out.println(conString[0]);
             rest = HttpConnetionExample.conect(conString);
             System.out.println(rest + "<-----");
@@ -28,7 +25,7 @@ public class Serverfacade {
             String rest;
             String list = req.queryParamsValues("list")[0];
             String value = req.queryParamsValues("value")[0];
-            String[] conString = {"http://localhost:5000/binarysearch?list="+ list +"&value=" + value, "http://localhost:5000/binarysearch?list="+ list +"&value=" + value};
+            String[] conString = {"http://ec2-user@ec2-3-90-110-121.compute-1.amazonaws.com:5000/binarysearch?list="+ list +"&value=" + value, "http://ec2-user@ec2-107-21-15-250.compute-1.amazonaws.com:5000/binarysearch?list="+ list +"&value=" + value};
             rest = HttpConnetionExample.conect(conString);
             System.out.println(rest + "<-----");
             return rest;
